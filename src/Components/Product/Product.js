e20504cb26729888f4d66239bd9e934c1f326537
MilonMridha <milonmridha570@gmail.com>
2022-03-25 10:42:51 -0700

    added-fontawesome

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,3 +1,5 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Product = (props) => {
@@ -5,15 +7,15 @@ const Product = (props) => {
     const {name, price, img} = product;
     return (
         <div>
-           <div className="card">
-  <img src={img} class="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Name : {name}</h5>
-    <h6 className="card-title">Price : {price}</h6>
+            <div className="card mt-5">
+                <img src={img} class="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Name : {name}</h5>
+                 <h6 className="card-title">Price : {price}</h6>
    
-    <a href="/" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                <button href="/" className="btn btn-success text-dark ">Add To Cart <span><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></span></button>
+        </div>
+    </div>
         </div>
     );
 };
