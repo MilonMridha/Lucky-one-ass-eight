d26219eb3c06e12a410870b5242a735200af8055
MilonMridha <milonmridha570@gmail.com>
2022-03-25 10:31:21 -0700

    added-components

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Product = (props) => {
+    const {product} = props;
+    const {name, price, img} = product;
     return (
         <div>
-            <h5>this is product</h5>
+           <div className="card">
+  <img src={img} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Name : {name}</h5>
+    <h6 className="card-title">Price : {price}</h6>
+   
+    <a href="/" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
         </div>
     );
 };
